e825a553d3b250a23775d6dd30247e903fee4219
leo <liang.hao.1018@outlook.com>
2016-06-02 16:07:42 +0800

    添加项目文件。

---
+++ pictureProcess/Form1.cs
@@ -0,0 +1,417 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pictureProcess
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.pictureBox1.Image = new Bitmap(@"C:\Users\haoliang\Desktop\picture\omeoclickimg (17).jpg");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.pictureBox1.Image = GTo2Bit(new Bitmap(this.pictureBox1.Image));
+
+            this.pictureBox1.Image.Save(@"C:\Users\haoliang\Desktop\newpicture\" + Guid.NewGuid().ToString("n") + ".jpg");
+        }
+
+        #region 二值化
+        /*
+        1位深度图像 颜色表数组255个元素 只有用前两个 0对应0  1对应255 
+        1位深度图像每个像素占一位
+        8位深度图像每个像素占一个字节  是1位的8倍
+        */
+        /// <summary>
+        /// 将源灰度图像二值化，并转化为1位二值图像。
+        /// </summary>
+        /// <param name="bmp"> 源灰度图像。 </param>
+        /// <returns> 1位二值图像。 </returns>
+        public static Bitmap GTo2Bit(Bitmap bmp)
+        {
+            if (bmp != null)
+            {
+                // 将源图像内存区域锁定
+                Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+                BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadOnly,
+                        PixelFormat.Format8bppIndexed);
+
+                // 获取图像参数
+                int leng, offset_1bit = 0;
+                int width = bmpData.Width;
+                int height = bmpData.Height;
+                int stride = bmpData.Stride;  // 扫描线的宽度,比实际图片要大
+                int offset = stride - width;  // 显示宽度与扫描线宽度的间隙
+                IntPtr ptr = bmpData.Scan0;   // 获取bmpData的内存起始位置的指针
+                int scanBytesLength = stride * height;  // 用stride宽度，表示这是内存区域的大小
+                if (width % 32 == 0)
+                {
+                    leng = width / 8;
+                }
+                else
+                {
+                    leng = width / 8 + (4 - (width / 8 % 4));
+                    if (width % 8 != 0)
+                    {
+                        offset_1bit = leng - width / 8;
+                    }
+                    else
+                    {
+                        offset_1bit = leng - width / 8;
+                    }
+                }
+
+                // 分别设置两个位置指针，指向源数组和目标数组
+                int posScan = 0, posDst = 0;
+                byte[] rgbValues = new byte[scanBytesLength];  // 为目标数组分配内存
+                Marshal.Copy(ptr, rgbValues, 0, scanBytesLength);  // 将图像数据拷贝到rgbValues中
+                // 分配二值数组
+                byte[] grayValues = new byte[leng * height]; // 不含未用空间。
+                // 计算二值数组
+                int x, v, t = 0;
+                for (int i = 0; i < height; i++)
+                {
+                    for (x = 0; x < width; x++)
+                    {
+                        v = rgbValues[posScan];
+                        t = (t << 1) | (v > 100 ? 1 : 0);
+
+
+                        if (x % 8 == 7)
+                        {
+                            grayValues[posDst] = (byte)t;
+                            posDst++;
+                            t = 0;
+                        }
+                        posScan++;
+                    }
+
+                    if ((x %= 8) != 7)
+                    {
+                        t <<= 8 - x;
+                        grayValues[posDst] = (byte)t;
+                    }
+                    // 跳过图像数据每行未用空间的字节，length = stride - width * bytePerPixel
+                    posScan += offset;
+                    posDst += offset_1bit;
+                }
+
+                // 内存解锁
+                Marshal.Copy(rgbValues, 0, ptr, scanBytesLength);
+                bmp.UnlockBits(bmpData);  // 解锁内存区域
+
+                // 构建1位二值位图
+                Bitmap retBitmap = twoBit(grayValues, width, height);
+                return retBitmap;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 用二值数组新建一个1位二值图像。
+        /// </summary>
+        /// <param name="rawValues"> 二值数组(length = width * height)。 </param>
+        /// <param name="width"> 图像宽度。 </param>
+        /// <param name="height"> 图像高度。 </param>
+        /// <returns> 新建的1位二值位图。 </returns>
+        private static Bitmap twoBit(byte[] rawValues, int width, int height)
+        {
+            // 新建一个1位二值位图，并锁定内存区域操作
+            Bitmap bitmap = new Bitmap(width, height, PixelFormat.Format1bppIndexed);
+            BitmapData bmpData = bitmap.LockBits(new Rectangle(0, 0, width, height),
+                 ImageLockMode.WriteOnly, PixelFormat.Format1bppIndexed);
+
+            // 计算图像参数
+            int offset = bmpData.Stride - bmpData.Width / 8;        // 计算每行未用空间字节数
+            IntPtr ptr = bmpData.Scan0;                         // 获取首地址
+            int scanBytes = bmpData.Stride * bmpData.Height;    // 图像字节数 = 扫描字节数 * 高度
+            byte[] grayValues = new byte[scanBytes];            // 为图像数据分配内存
+
+            // 为图像数据赋值
+            int posScan = 0;                        // rawValues和grayValues的索引
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < bmpData.Width / 8; j++)
+                {
+                    grayValues[posScan] = rawValues[posScan];
+                    posScan++;
+                }
+                // 跳过图像数据每行未用空间的字节，length = stride - width * bytePerPixel
+                posScan += offset;
+            }
+
+            // 内存解锁
+            Marshal.Copy(grayValues, 0, ptr, scanBytes);
+            bitmap.UnlockBits(bmpData);  // 解锁内存区域
+
+            // 修改生成位图的索引表
+            ColorPalette palette;
+            // 获取一个Format8bppIndexed格式图像的Palette对象
+            using (Bitmap bmp = new Bitmap(1, 1, PixelFormat.Format1bppIndexed))
+            {
+                palette = bmp.Palette;
+            }
+            for (int i = 0; i < 2; i = +254)
+            {
+                palette.Entries[i] = Color.FromArgb(i, i, i);
+            }
+            // 修改生成位图的索引表
+            bitmap.Palette = palette;
+
+            return bitmap;
+        }
+        #endregion
+
+        private void DiPian()
+        {
+            //以底片效果显示图像
+            try
+            {
+                int Height = this.pictureBox1.Image.Height;
+                int Width = this.pictureBox1.Image.Width;
+                Bitmap newbitmap = new Bitmap(Width, Height);
+                Bitmap oldbitmap = (Bitmap)this.pictureBox1.Image;
+                Color pixel;
+                for (int x = 1; x < Width; x++)
+                {
+                    for (int y = 1; y < Height; y++)
+                    {
+                        int r, g, b;
+                        pixel = oldbitmap.GetPixel(x, y);
+                        r = 255 - pixel.R;
+                        g = 255 - pixel.G;
+                        b = 255 - pixel.B;
+                        newbitmap.SetPixel(x, y, Color.FromArgb(r, g, b));
+                    }
+                }
+                this.pictureBox1.Image = newbitmap;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "信息提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void FuDiao()
+        {
+            //以浮雕效果显示图像
+            try
+            {
+                int Height = this.pictureBox1.Image.Height;
+                int Width = this.pictureBox1.Image.Width;
+                Bitmap newBitmap = new Bitmap(Width, Height);
+                Bitmap oldBitmap = (Bitmap)this.pictureBox1.Image;
+                Color pixel1, pixel2;
+                for (int x = 0; x < Width - 1; x++)
+                {
+                    for (int y = 0; y < Height - 1; y++)
+                    {
+                        int r = 0, g = 0, b = 0;
+                        pixel1 = oldBitmap.GetPixel(x, y);
+                        pixel2 = oldBitmap.GetPixel(x + 1, y + 1);
+                        r = Math.Abs(pixel1.R - pixel2.R + 128);
+                        g = Math.Abs(pixel1.G - pixel2.G + 128);
+                        b = Math.Abs(pixel1.B - pixel2.B + 128);
+                        if (r > 255)
+                            r = 255;
+                        if (r < 0)
+                            r = 0;
+                        if (g > 255)
+                            g = 255;
+                        if (g < 0)
+                            g = 0;
+                        if (b > 255)
+                            b = 255;
+                        if (b < 0)
+                            b = 0;
+                        newBitmap.SetPixel(x, y, Color.FromArgb(r, g, b));
+                    }
+                }
+                this.pictureBox1.Image = newBitmap;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "信息提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void HeiBai()
+        {
+            //以黑白效果显示图像
+            try
+            {
+                int Height = this.pictureBox1.Image.Height;
+                int Width = this.pictureBox1.Image.Width;
+                Bitmap newBitmap = new Bitmap(Width, Height);
+                Bitmap oldBitmap = (Bitmap)this.pictureBox1.Image;
+                Color pixel;
+                for (int x = 0; x < Width; x++)
+                    for (int y = 0; y < Height; y++)
+                    {
+                        pixel = oldBitmap.GetPixel(x, y);
+                        int r, g, b, Result = 0;
+                        r = pixel.R;
+                        g = pixel.G;
+                        b = pixel.B;
+                        //实例程序以加权平均值法产生黑白图像
+                        int iType = 2;
+                        switch (iType)
+                        {
+                            case 0://平均值法
+                                Result = ((r + g + b) / 3);
+                                break;
+                            case 1://最大值法
+                                Result = r > g ? r : g;
+                                Result = Result > b ? Result : b;
+                                break;
+                            case 2://加权平均值法
+                                Result = ((int)(0.7 * r) + (int)(0.2 * g) + (int)(0.1 * b));
+                                break;
+                        }
+                        newBitmap.SetPixel(x, y, Color.FromArgb(Result, Result, Result));
+                    }
+                this.pictureBox1.Image = newBitmap;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "信息提示");
+            }
+        }
+
+        private void RoHua()
+        {
+            //以柔化效果显示图像
+            try
+            {
+                int Height = this.pictureBox1.Image.Height;
+                int Width = this.pictureBox1.Image.Width;
+                Bitmap bitmap = new Bitmap(Width, Height);
+                Bitmap MyBitmap = (Bitmap)this.pictureBox1.Image;
+                Color pixel;
+                //高斯模板
+                int[] Gauss = { 1, 2, 1, 2, 4, 2, 1, 2, 1 };
+                for (int x = 1; x < Width - 1; x++)
+                    for (int y = 1; y < Height - 1; y++)
+                    {
+                        int r = 0, g = 0, b = 0;
+                        int Index = 0;
+                        for (int col = -1; col <= 1; col++)
+                            for (int row = -1; row <= 1; row++)
+                            {
+                                pixel = MyBitmap.GetPixel(x + row, y + col);
+                                r += pixel.R * Gauss[Index];
+                                g += pixel.G * Gauss[Index];
+                                b += pixel.B * Gauss[Index];
+                                Index++;
+                            }
+                        r /= 16;
+                        g /= 16;
+                        b /= 16;
+                        //处理颜色值溢出
+                        r = r > 255 ? 255 : r;
+                        r = r < 0 ? 0 : r;
+                        g = g > 255 ? 255 : g;
+                        g = g < 0 ? 0 : g;
+                        b = b > 255 ? 255 : b;
+                        b = b < 0 ? 0 : b;
+                        bitmap.SetPixel(x - 1, y - 1, Color.FromArgb(r, g, b));
+                    }
+                this.pictureBox1.Image = bitmap;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "信息提示");
+            }
+        }
+
+        private void RuiHua()
+        {
+            //以锐化效果显示图像
+            try
+            {
+                int Height = this.pictureBox1.Image.Height;
+                int Width = this.pictureBox1.Image.Width;
+                Bitmap newBitmap = new Bitmap(Width, Height);
+                Bitmap oldBitmap = (Bitmap)this.pictureBox1.Image;
+                Color pixel;
+                //拉普拉斯模板
+                int[] Laplacian = { -1, -1, -1, -1, 9, -1, -1, -1, -1 };
+                for (int x = 1; x < Width - 1; x++)
+                    for (int y = 1; y < Height - 1; y++)
+                    {
+                        int r = 0, g = 0, b = 0;
+                        int Index = 0;
+                        for (int col = -1; col <= 1; col++)
+                            for (int row = -1; row <= 1; row++)
+                            {
+                                pixel = oldBitmap.GetPixel(x + row, y + col); r += pixel.R * Laplacian[Index];
+                                g += pixel.G * Laplacian[Index];
+                                b += pixel.B * Laplacian[Index];
+                                Index++;
+                            }
+                        //处理颜色值溢出
+                        r = r > 255 ? 255 : r;
+                        r = r < 0 ? 0 : r;
+                        g = g > 255 ? 255 : g;
+                        g = g < 0 ? 0 : g;
+                        b = b > 255 ? 255 : b;
+                        b = b < 0 ? 0 : b;
+                        newBitmap.SetPixel(x - 1, y - 1, Color.FromArgb(r, g, b));
+                    }
+                this.pictureBox1.Image = newBitmap;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "信息提示");
+            }
+        }
+
+        private void WuHua()
+        {
+            //以雾化效果显示图像
+            try
+            {
+                int Height = this.pictureBox1.Image.Height;
+                int Width = this.pictureBox1.Image.Width;
+                Bitmap newBitmap = new Bitmap(Width, Height);
+                Bitmap oldBitmap = (Bitmap)this.pictureBox1.Image;
+                Color pixel;
+                for (int x = 1; x < Width - 1; x++)
+                    for (int y = 1; y < Height - 1; y++)
+                    {
+                        System.Random MyRandom = new Random();
+                        int k = MyRandom.Next(123456);
+                        //像素块大小
+                        int dx = x + k % 19;
+                        int dy = y + k % 19;
+                        if (dx >= Width)
+                            dx = Width - 1;
+                        if (dy >= Height)
+                            dy = Height - 1;
+                        pixel = oldBitmap.GetPixel(dx, dy);
+                        newBitmap.SetPixel(x, y, pixel);
+                    }
+                this.pictureBox1.Image = newBitmap;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "信息提示");
+            }
+        }
+    }
+}
